3731dd35b62b12d0abdf6a4721c54539dfd3a1ba
AlekseyLagutkin <lagutkin@yahoo.com>
2023-05-10 23:37:48 +0300

    866

--- user.h
+++ user.h
@@ -14,19 +14,19 @@ public:
 	string get_login();
 	string get_pasw();
 	string get_name();
-	void show_mes();   //┬√тюф юЄяЁртыхээ√ї ш тїюф ∙шї ёююс∙хэшщ
+	void show_mes();   //Вывод отправленных и входящих сообщений
 	string qw;
 	~user();
 private:
 	string login;
 	string pasw;
 	string name;
-	string sent_1;   //╬ЄяЁртыхээюх ёююс∙хэшх 1
-	string sent_2;   //╬ЄяЁртыхээюх ёююс∙хэшх 2
-	string to_1;     //╩юьє юЄяЁртыхэю ёююс∙хэшх 1
-	string to_2;     //╩юьє юЄяЁртыхэю ёююс∙хэшх 2
-	string rec_1;    //╧Ёшэ Єюх ёююс∙хэшх 1
-	string rec_2;    //╧Ёшэ Єюх ёююс∙хэшх 2
-	string from_1;   //╬Є ъюую яЁшэ Єю ёююс∙хэшх 1
-	string from_2;   //╬Є ъюую яЁшэ Єю ёююс∙хэшх 2
+	string sent_1;   //Отправленное сообщение 1
+	string sent_2;   //Отправленное сообщение 2
+	string to_1;     //Кому отправлено сообщение 1
+	string to_2;     //Кому отправлено сообщение 2
+	string rec_1;    //Принятое сообщение 1
+	string rec_2;    //Принятое сообщение 2
+	string from_1;   //От кого принято сообщение 1
+	string from_2;   //От кого принято сообщение 2
 };
